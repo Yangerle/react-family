840a425f972893c1e88112121d110717affe4e59
Yangerle <1136510804@qq.com>
2019-03-13 17:08:27 +0800

    文件路径优化

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,6 +8,13 @@ module.exports={
 		path:path.join(__dirname,'./dist'),
 		filename:'bundle.js'
 	},
+	resolve: {
+		alias: {
+			pages: path.join(__dirname, 'src/pages'),
+			component: path.join(__dirname, 'src/component'),
+			router: path.join(__dirname, 'src/router')
+		}
+	},
 	devServer:{
 		port: 8080,
 		contentBase: path.join(__dirname, './dist'),
