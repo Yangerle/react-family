e7fda269f3f0ea4727ba513bc9d0e21af436e10d
Yangerle <1136510804@qq.com>
2019-03-14 18:12:16 +0800

    优化缓存

--- webpack.config.js
+++ webpack.config.js
@@ -34,12 +34,17 @@ module.exports = {
 		new webpack.optimize.CommonsChunkPlugin({
 			name: 'vendor'
 		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'runtime'
+		}),
 		new UglifyJSPlugin(),
 		new webpack.DefinePlugin({
 			'process.env': {
 				'NODE_ENV': JSON.stringify('production')
 			}
-		})
+		}),
+		new webpack.HashedModuleIdsPlugin()
+
 
 	],
 	module: {
