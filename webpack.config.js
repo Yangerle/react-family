0b01db67d0a35a82738ffcaf93a2fae17fcdd060
Yangerle <1136510804@qq.com>
2019-03-14 18:00:08 +0800

    生产环境构建

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+	devtool: 'cheap-module-source-map',
+	entry: {
+		app: [
+			path.join(__dirname, 'src/index.js')
+		],
+		vendor: ['react', 'react-router-dom', 'redux', 'react-dom', 'react-redux']
+	},
+	output: {
+		path: path.join(__dirname, './dist'),
+		filename: '[name].[chunkhash].js',
+		chunkFilename: '[name].[chunkhash].js'
+	},
+	resolve: {
+		alias: {
+			pages: path.join(__dirname, 'src/pages'),
+			component: path.join(__dirname, 'src/component'),
+			router: path.join(__dirname, 'src/router'),
+			// redux: path.join(__dirname, 'src/redux')//不能直接使用别名redux,因为‘import {createStore} from 'redux'’语句在webapck编译的时候碰到redux都去src/redux去找了，找不着，问题就出现了
+			reduxs: path.join(__dirname, 'src/redux')
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: path.join(__dirname, 'src/index.html')
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor'
+		})
+	],
+	module: {
+		rules: [{
+			test: /\.js$/,
+			use: ['babel-loader'],
+			include: path.join(__dirname, 'src')
+		}, {
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		}, {
+			test: /\.(png|jpg|gif)$/,
+			use: [{
+				loader: 'url-loader',
+				options: {
+					limit: 8192
+				}
+			}]
+		}]
+	}
+};
