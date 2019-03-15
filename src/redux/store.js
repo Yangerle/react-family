e18081a4d3fb17f533d2c3221ab995bdccef19de
Yangerle <1136510804@qq.com>
2019-03-15 10:08:04 +0800

    使用axios和middleware优化API请求

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,9 @@ import {createStore,applyMiddleware} from 'redux';
 import combineReducers from './reducers.js';
 import thunkMiddleware from 'redux-thunk';
 
-let store = createStore(combineReducers,applyMiddleware(thunkMiddleware));
+import promiseMiddleware from './middleware/promiseMiddleware'
+
+
+let store = createStore(combineReducers,applyMiddleware(thunkMiddleware,promiseMiddleware));
 
 export default store;//action与reducer的沟通者，提供沟通方法
