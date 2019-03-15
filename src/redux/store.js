ec4349877feb3d7e018f7342d4ef7248efc7ec3b
Yangerle <1136510804@qq.com>
2019-03-15 13:58:38 +0800

    redux 模块热替换配置-reducer改变整个页面会刷新问题解决

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,12 @@ import thunkMiddleware from 'redux-thunk';
 
 import promiseMiddleware from './middleware/promiseMiddleware'
 
-
+if (module.hot) {
+	module.hot.accept("./reducers", () => {
+		const nextCombineReducers = require("./reducers").default;
+		store.replaceReducer(nextCombineReducers);
+	});
+}
 let store = createStore(combineReducers,applyMiddleware(thunkMiddleware,promiseMiddleware));
 
 export default store;//action与reducer的沟通者，提供沟通方法
